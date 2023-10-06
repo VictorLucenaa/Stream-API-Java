c4b5ae7ab5ce97e87d1c25ea4be241c678ffe911
Victor Lucena <victor.lucenaaa@gmail.com>
2023-10-06 13:06:55 -0300

    adiciona exemplos de stream-API

---
+++ src/main/java/functional_interface/examples/PredicateExample.java
@@ -0,0 +1,19 @@
+package src.main.java.functional_interface.examples;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class PredicateExample {
+
+    public static void main(String[] args) {
+
+        List<String> palavras = Arrays.asList("Java", "kotlin", "python", "C", "go", "ruby", "javascript");
+
+//        Predicate<String> maisDeCincoCaracteres = palavra -> palavra.length() > 5;
+
+        palavras.stream().filter(p -> p.length() > 5).forEach(System.out::println);
+    }
+
+
+}
